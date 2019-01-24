63f1f0dda24e8e034a6662cc6256fca39987dac1
Kamila Sieczkowska <kamila.sieczkowska@closbrothers.pl>
2019-01-24 17:13:02 +0100

    with mobile menu animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,68 @@
 $( document ).ready(function() {
 
 
-
-    function toggleMenu(){
-        if ($(this).hasClass('is-active')){
-            $(this).removeClass('is-active');
-            $('#menu').removeClass('active-menu');
-        }
-        else {
-            $(this).addClass('is-active');
-            $('#menu').addClass('active-menu');
-        }
-    }
-
-    $('#mobile-menu').on('click', function(){
-        toggleMenu();
-    });
+        // TOGGLE MOBILE MENU
+        $('#toggle').click(function () {
+            $(this).toggleClass('active');
+            $('#menu').toggleClass('open');
+        });
     
-    $('.nav__list li a').each(function(){
-        var el = $(this);
-        var target = el.attr('href');
-
-        el.click(function(){
-            // target.animatescroll({scrollSpeed: ($(this)).attr('data-speed')});
-            $('#menu').removeClass('active-menu');
+        //HIDE MOBILE MENU ON ITEM CLICK
+        var mobileMenuLinks = $('#mobile__menu').find('.menu__item');
+        mobileMenuLinks.each(function () {
+            $(this).click(function () {
+                if ($('#toggle').hasClass('active')) {
+                    setTimeout(function () {
+                        $('#toggle').removeClass('active');
+                        $('#mobile__menu').removeClass('open');
+                    }, 300)
+                }
+            })
         })
-    });
+    
+        // SCROLL NAVI EFFECT
+        $(document).scroll(function () {
+            var $nav = $(".navbar");
+            $nav.toggleClass('after-scroll', $(this).scrollTop() > $nav.height());
+        });
+    
+        // SCROLL TO ID
+        // Select all links with hashes
+        $('a[href*="#"]')
+            // Remove links that don't actually link to anything
+            .not('[href="#"]')
+            .not('[href="#0"]')
+            .click(function (event) {
+                // On-page links
+                if (
+                    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                    &&
+                    location.hostname == this.hostname
+                ) {
+                    // Figure out element to scroll to
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    // Does a scroll target exist?
+                    if (target.length) {
+                        // Only prevent default if animation is actually gonna happen
+                        event.preventDefault();
+                        $('html, body').animate({
+                            scrollTop: (target.offset().top) - 90
+                        }, 1000, function () {
+                            // Callback after animation
+                            // Must change focus!
+                            var $target = $(target);
+                            $target.focus();
+                            if ($target.is(":focus")) { // Checking if the target was focused
+                                return false;
+                            } else {
+                                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                                $target.focus(); // Set focus again
+                            };
+                        });
+                    }
+                }
+        });
 
         
     
